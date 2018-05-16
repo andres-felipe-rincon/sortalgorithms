99e2c2bd705fc63ba08f0a6491dd8b1c9eebe045
Andres Rincon <afrincon0@unadvirtual.edu.co>
2018-05-16 17:02:28 -0500

    changes on home

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $("nav").click(function () {
   $(this).toggleClass("active");
 });
+
+$(window).load(function() {
+  $('.card').hover(function() {
+    $(this).find('.description').stop().animate({
+      height: "toggle",
+      opacity: "toggle"
+    }, 300);
+  });
+});
